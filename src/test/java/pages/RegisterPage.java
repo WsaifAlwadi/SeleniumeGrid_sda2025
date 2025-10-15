9d0cb3d6d93ad5ebddb0741277a34ea18a0ef026
96659 <ws.aif616@icloud.com>
2025-10-15 20:15:10 +0300

    Add initial project structure with TestNg and selenium tests

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class RegisterPage extends BasePage {
-
+//rigesteration
     @FindBy(id = "firstName") private WebElement firstNameInput;
     @FindBy(id = "lastName") private WebElement lastNameInput;
     @FindBy(id = "email") private WebElement emailInput;
